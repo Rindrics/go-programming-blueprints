a731b516307c29a56f6544b1fffd8a9c693a4f70
Akira Hayashi <smxshxishxad@me.com>
2022-04-21 02:27:52 +0900

    :sparkles: Define templateHandler.ServeHTTP() to serve HTML from template

--- chat/main.go
+++ chat/main.go
@@ -13,6 +13,13 @@ type templateHandler struct {
 	templ    *template.Template
 }
 
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`
